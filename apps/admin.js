7ad1ef0684413f15af2d4b3a52a8fa9299bc3083
YooLan <61037787+Zacharvc@users.noreply.github.com>
2022-08-18 22:41:45 +0800

    修复admin.js的Yoolan日志

--- apps/admin.js
+++ apps/admin.js
@@ -1,6 +1,6 @@
 import {segment} from "oicq";
 import {exec} from "child_process";
-import {yoolanVersion, currentLog} from "../module/config.js";
+import {yoolanVersion, updateLog} from "../module/config.js";
 
 const _path = process.cwd();
 const _yoolanPluginPath = `${_path}/plugins/yoolan-plugin/`;
@@ -94,7 +94,7 @@ export class admin extends plugin{
 		if(!e.isMaster) return;
 		//
 		let replyMsg = [`当前版本: ${yoolanVersion}`];
-		for(let content of currentLog[yoolanVersion]) replyMsg.push(content);
+		for(let content of updateLog[yoolanVersion]) replyMsg.push(content);
 		//
 		e.reply(replyMsg.join("\n"));
 		//
