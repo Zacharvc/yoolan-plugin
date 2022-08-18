65fbd69a1267bb592d83e228da64df2200cce1c5
YooLan <61037787+Zacharvc@users.noreply.github.com>
2022-08-18 22:16:44 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ import {yoolanVersion} from "./module/config.js";
 //
 let initMsg = [
 	"---------------------",
-	`悠懒插件${yoolanVersion}初始化`,
-	"---------------------"
+	`悠懒插件${yoolanVersion}初始化`
 ];
 //打印日志
 for(let Amsg of initMsg) logger.info(Amsg);
