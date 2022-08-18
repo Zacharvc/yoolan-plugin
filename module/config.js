f215e3eae5918ce74eacca3c7a3d94df70322926
YooLan <61037787+Zacharvc@users.noreply.github.com>
2022-08-18 19:52:03 +0800

    Add files via upload

--- module/config.js
+++ module/config.js
@@ -4,15 +4,12 @@ import YAML from "yaml";
 const _path = process.cwd();
 const _logPath = `${_path}/plugins/yoolan-plugin/config/updateLog.yaml`;
 //
-let yoolanVersion = 0;
+let yoolanVersion = "0";
 //
-let packageJson = JSON.parse(fs.readFileSync("package.json", "utf8"));
 let updateLog = YAML.parse(fs.readFileSync(_logPath, "utf-8").toString());
 //
-yoolanVersion = Object.keys(updateLog.version)[0];
+yoolanVersion = Object.keys(updateLog.version)[0].toString();
 //
 let currentLog = updateLog.version[yoolanVersion].join("\n");
 //
-const isV3 = packageJson.version[0] === "3";
-//
-export default {yoolanVersion, currentLog, isV3};
+export {yoolanVersion, currentLog};
