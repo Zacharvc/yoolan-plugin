56703d9c25259dcb4e40427fd234cc00e3c9b092
YooLan <61037787+Zacharvc@users.noreply.github.com>
2022-08-20 15:21:49 +0800

    添加注释

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import fs from "fs";
 import {yoolanVersion} from "./module/config.js";
-//
+//初始化日志内容
 let initMsg = [
 	"---------------------",
 	`悠懒插件${yoolanVersion}初始化`
@@ -9,7 +9,7 @@ let initMsg = [
 for(let Amsg of initMsg) logger.info(Amsg);
 //
 let apps = {};
-//
+//读取文件列表
 const extensionsFile = fs.readdirSync("./plugins/yoolan-plugin/apps").filter((file) => file.endsWith(".js"));
 //
 for(let Afile of extensionsFile){
@@ -18,5 +18,5 @@ for(let Afile of extensionsFile){
 	//
 	apps[fileName] = (await import("./apps/" + Afile))[fileName];
 }
-//
+//添加到apps
 export {apps};
