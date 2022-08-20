27691e708cea0a8d7973c2aec211f7553bb75510
YooLan <61037787+Zacharvc@users.noreply.github.com>
2022-08-20 15:23:14 +0800

    改

--- module/config.js
+++ module/config.js
@@ -4,7 +4,7 @@ import YAML from "yaml";
 const _path = process.cwd();
 const _logPath = `${_path}/plugins/yoolan-plugin/config/updateLog.yaml`;
 //
-let yoolanVersion = "0";
+let yoolanVersion = "1.0.0";
 //
 let updateLog = YAML.parse(fs.readFileSync(_logPath, "utf-8").toString());
 //
